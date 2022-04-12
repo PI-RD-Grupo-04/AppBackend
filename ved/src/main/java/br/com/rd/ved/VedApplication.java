eb6cb305ebb3c63d2d4dcc2efae7540a226841c9
jefferson <guinho.psp1@gmail.com>
2022-04-12 15:33:36 -0300

    começando projeto

---
+++ ved/src/main/java/br/com/rd/ved/VedApplication.java
@@ -0,0 +1,13 @@
+package br.com.rd.ved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedApplication.class, args);
+	}
+
+}
