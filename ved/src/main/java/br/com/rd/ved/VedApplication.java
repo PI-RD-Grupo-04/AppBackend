bb3ee5fe974f5a41e08595cec5e4f8183fbb36d8
Caroline Melo <2015302@aluno.univesp.br>
2022-04-20 22:54:08 -0300

    crud fornecedor

--- ved/src/main/java/br/com/rd/ved/VedApplication.java
+++ ved/src/main/java/br/com/rd/ved/VedApplication.java
@@ -7,19 +7,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.rd.ved.service.ClienteService;
+import br.com.rd.ved.service.FornecedorService;
 
 @SpringBootApplication
 public class VedApplication implements CommandLineRunner {
 
 	private ClienteService clienteService;
+	private FornecedorService fornecedorService;
 	private Boolean sistema = true;
 
-	public VedApplication(ClienteService clienteService) {
+	public VedApplication(ClienteService clienteService, FornecedorService fornecedorService) {
 		this.clienteService = clienteService;
-	
+		this.fornecedorService = fornecedorService;
+
 	}
-	
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(VedApplication.class, args);
@@ -34,18 +35,18 @@ public class VedApplication implements CommandLineRunner {
 			System.out.println("Qual a ação que será executada?");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Gestão de Clientes");
-			
+			System.out.println("2 - Gestão de Fornecedores");
 
 			acao = sc.nextInt();
 			if (acao == 1) {
 				clienteService.iniciar(sc);
+			} else if (acao == 2) {
+				fornecedorService.iniciar(sc);
 
 			} else {
 				sistema = false;
 			}
 		}
 		sc.close();
-
 	}
-
 }
--- ved/src/main/java/br/com/rd/ved/model/Fornecedor.java
+++ ved/src/main/java/br/com/rd/ved/model/Fornecedor.java
@@ -1,86 +1,83 @@
-//package br.com.rd.ved.model;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//import javax.validation.constraints.Size;
-//
-//@Entity
-//@Table(name="fornecedores")
-//public class Fornecedor {
-//
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	@Column(name="id_fornecedor")
-//	private Integer id;
-//	@Column(name="razao_social")
-//	@Size(max = 50)
-//	private String razaoSocial;
-//	@Column(name="cnpj")
-//	@Size(max = 20)
-//	private String cnpj;
-//	@Column(name="email")
-//	@Size(max = 30)
-//	private String email;
-//	
-//	public Fornecedor() {
-//		super();
-//	}
-//
-//	public Fornecedor(Integer id, String razaoSocial, String cnpj, String email) {
-//		this.id = id;
-//		this.razaoSocial = razaoSocial;
-//		this.cnpj = cnpj;
-//		this.email = email;
-//	}
-//
-//	public Fornecedor(String razaoSocial, String cnpj, String email) {
-//		this.razaoSocial = razaoSocial;
-//		this.cnpj = cnpj;
-//		this.email = email;
-//	}
-//
-//	public Integer getId() {
-//		return id;
-//	}
-//
-//	public void setId(Integer id) {
-//		this.id = id;
-//	}
-//
-//	public String getRazaoSocial() {
-//		return razaoSocial;
-//	}
-//
-//	public void setRazaoSocial(String razaoSocial) {
-//		this.razaoSocial = razaoSocial;
-//	}
-//
-//	public String getCnpj() {
-//		return cnpj;
-//	}
-//
-//	public void setCnpj(String cnpj) {
-//		this.cnpj = cnpj;
-//	}
-//
-//	public String getEmail() {
-//		return email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Fornecedor [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", email=" + email + "]";
-//	}
-//	
-//	
-//	
-//	
-//}
+package br.com.rd.ved.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "fornecedor")
+public class Fornecedor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_fornecedor")
+	private Integer id;
+	@Column(name = "razao_social")
+	@Size(max = 50)
+	private String razaoSocial;
+	@Column(name = "cnpj")
+	@Size(max = 20)
+	private String cnpj;
+	@Column(name = "email")
+	@Size(max = 30)
+	private String email;
+
+	public Fornecedor() {
+		super();
+	}
+
+	public Fornecedor(Integer id, String razaoSocial, String cnpj, String email) {
+		this.id = id;
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.email = email;
+	}
+
+	public Fornecedor(String razaoSocial, String cnpj, String email) {
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Fornecedor [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", email=" + email + "]";
+	}
+
+}
