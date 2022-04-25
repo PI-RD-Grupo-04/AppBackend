e3c2c0dee062918c07739fe36d64cd5ce515bbbd
Will <wb.premiertech@gmail.com>
2022-04-25 16:03:30 -0300

    Corrigindo o erro que quebra aplicação

--- ved/src/main/java/br/com/rd/ved/model/Categoria.java
+++ ved/src/main/java/br/com/rd/ved/model/Categoria.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -24,7 +24,7 @@ public class Categoria {
 	private String descricao;
 	
 	
-	@ManyToMany(mappedBy="categoria")
+	@OneToMany(mappedBy="categoria")
 	private List <Produto> produtos;
 	
 	
