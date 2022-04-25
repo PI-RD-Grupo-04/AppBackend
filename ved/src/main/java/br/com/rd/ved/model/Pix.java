abe2f490baa2692b6129c46bf798f1a07aff33ad
washington <washingtondejesuspereira@hotmail.com>
2022-04-25 11:21:32 -0300

    washington

---
+++ ved/src/main/java/br/com/rd/ved/model/Pix.java
@@ -0,0 +1,57 @@
+package br.com.rd.ved.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pix")
+public class Pix {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_pix")
+	private Integer id;
+	@Column(name="codigo_pix")
+	private String codigoPix;
+	
+	public Pix() {
+		super();
+	}
+
+	public Pix(Integer id, String codigoPix) {
+		this.id = id;
+		this.codigoPix = codigoPix;
+	}
+
+	public Pix(String codigoPix) {
+		this.codigoPix = codigoPix;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCodigoPix() {
+		return codigoPix;
+	}
+
+	public void setCodigoPix(String codigoPix) {
+		this.codigoPix = codigoPix;
+	}
+
+	@Override
+	public String toString() {
+		return "Pix [id=" + id + ", codigoPix=" + codigoPix + "]";
+	}
+	
+	
+	
+}
