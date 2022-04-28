55499bd6abf9cb9d9d782c56c23a3ce02deeb538
washington <washingtondejesuspereira@hotmail.com>
2022-04-28 20:01:44 -0300

    washington

---
+++ ved/src/main/java/br/com/rd/ved/controller/ClienteController.java
@@ -0,0 +1,27 @@
+package br.com.rd.ved.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rd.ved.model.Cliente;
+import br.com.rd.ved.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
+	public List<Cliente> Listar() {
+
+		List<Cliente> clientes = clienteRepository.findAll();
+		return clientes;
+	}
+
+}
