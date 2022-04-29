afcb9b315160fc41884b4b5da75477b799f79758
kotejg <guinho.psp1@gmail.com>
2022-04-29 20:15:24 -0300

    criando metodo de deletar cliente

--- ved/src/main/java/br/com/rd/ved/controller/ClienteController.java
+++ ved/src/main/java/br/com/rd/ved/controller/ClienteController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,20 @@ public class ClienteController {
 		}
 
 		return ResponseEntity.notFound().build();
+	} 
+	
+	
+	@DeleteMapping("/delete={id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable("id") Integer id){
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		
+		if(cliente.isPresent()) {
+			clienteRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+		
 	}
 
 }
