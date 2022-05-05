8c5d59a9d6ccd2b19bccd262ec78eb7ddbdd961d
Will <wb.premiertech@gmail.com>
2022-05-05 11:10:28 -0300

    Subindo item pedido

---
+++ ved/src/main/java/br/com/rd/ved/formdto/ItemPedidoForm.java
@@ -0,0 +1,20 @@
+package br.com.rd.ved.formdto;
+
+import br.com.rd.ved.model.ItemPedido;
+import br.com.rd.ved.model.Pedido;
+import br.com.rd.ved.repository.PedidoRepository;
+import br.com.rd.ved.repository.ProdutoRepository;
+
+public class ItemPedidoForm {
+
+	public void cadastrarItemPedido(ItemPedido itemPedido, Pedido pedido, PedidoRepository pedidoRepository) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ItemPedido converter(ProdutoRepository produtoRepository) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
