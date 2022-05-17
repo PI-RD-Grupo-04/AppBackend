141c7bb1a072c8831d01240d1de129c61864c93e
Caroline Melo <2015302@aluno.univesp.br>
2022-05-17 09:35:20 -0300

    planos

--- ved/src/main/java/br/com/rd/ved/repository/ProdutoRepository.java
+++ ved/src/main/java/br/com/rd/ved/repository/ProdutoRepository.java
@@ -1,13 +1,11 @@
 package br.com.rd.ved.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import br.com.rd.ved.model.Produto;
 
 @Repository
@@ -18,18 +16,15 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer>, Crud
 
 	@Query(value = "select * from produto p inner join categoria c on p.id_categoria = c.id_categoria"
 			+ " where c.descricao_categoria = :categoria order by id_produto desc limit 4 ", nativeQuery = true)
-	List<Produto> findProdutosPorCategoria(@Param("categoria")String categoria); 
-	
+	List<Produto> findProdutosPorCategoria(@Param("categoria") String categoria);
+
 	@Query(value = "select * from produto p order by p.id_produto desc limit 8", nativeQuery = true)
-	List<Produto> findNovidades();  
-	
+	List<Produto> findNovidades();
+
 	@Query(value = " select * from produto where preco <= :valor", nativeQuery = true)
-	List<Produto> findProdutosPorValor(@Param("valor")Integer valor);  
-	
-	
+	List<Produto> findProdutosPorValor(@Param("valor") Integer valor);
+
 	@Query(value = " select * from produto where nome_produto like :palavra%", nativeQuery = true)
-	List<Produto> findProdutosPorPalavra(@Param("palavra")String palavra); 
-	
-	}
+	List<Produto> findProdutosPorPalavra(@Param("palavra") String palavra);
 
-
+}
